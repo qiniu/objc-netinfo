c18b377d0daaf454250df25d898feb6f6b4ae9df
longbai <slongbai@gmail.com>
2016-01-28 02:14:05 +0800

    rename pod

--- NetworkInfo/QNNetworkInfo.h
+++ NetworkInfo/QNNetworkInfo.h
@@ -8,20 +8,55 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *    Network Type
+ */
 typedef NS_ENUM(NSUInteger, QNNetWorkType) {
+    /**
+     *    No network.
+     */
     QNNetWorkType_None = 0,
+    /**
+     *    WIFI network
+     */
     QNNetWorkType_WIFI,
+    /**
+     *    Mobile network(2G/3G/4G/...)
+     */
     QNNetWorkType_MOBILE,
 };
 
+/**
+ *    Namespace
+ */
 @interface QNNetworkInfo : NSObject
 
+/**
+ *    the ipv4 of the device
+ *
+ *    @return ipv4 string
+ */
 + (NSString *)deviceIP;
 
+/**
+ *    the dns server list of the device, ipv4
+ *
+ *    @return ipv4 string list
+ */
 + (NSArray *)localDNSServers;
 
+/**
+ *    networktype
+ *
+ *    @return networktype enum
+ */
 + (QNNetWorkType)networkType;
 
+/**
+ *    network description
+ *
+ *    @return wife/evdo/gsm/...
+ */
 + (NSString *)networkDescription;
 
 @end
